c2ca0b7acffe983d7eee37d90b7e25094257e95b
johnkmaina <info@softiqtechnologies.co.ke>
2021-01-01 21:04:23 +0300

    added test_msisdn

---
+++ keys.py
@@ -0,0 +1,7 @@
+business_shortCode = "174379"
+phone_number = "254725314691"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "IuQUGEdCcvf7wPhi9f8nfcnPG9gG9QV9"
+consumer_secret = "yodajtZ8AB8MWYkd"
+shortcode = "600277"
+test_msisdn = "254708374149"
